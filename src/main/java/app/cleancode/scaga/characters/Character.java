a1c5eb45a0e5faafc60be063663343aeb4984f9a
Jett Thompson <jett.thompson.59@gmail.com>
2021-05-05 11:48:15 +1000

    fixed a compile error

--- src/main/java/app/cleancode/scaga/characters/Character.java
+++ src/main/java/app/cleancode/scaga/characters/Character.java
@@ -7,11 +7,9 @@ import app.cleancode.scaga.animation.Animation;
 
 public class Character {
 private final Map<String, Animation> animations;
-private final State [] possibleStates;
 
 public Character(String characterName) {
 	animations = new HashMap<>();
-	this.possibleStates = possibleStates;
 }
 
 public static enum State {
