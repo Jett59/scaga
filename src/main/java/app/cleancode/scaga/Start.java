5695b1bb57b09f2836ae762878b70f84245f23b9
Jett Thompson <jett.thompson.59@gmail.com>
2021-05-07 07:29:56 +1000

    added an annotation that will be used to designate a field as being from the game engine

--- src/main/java/app/cleancode/scaga/Start.java
+++ src/main/java/app/cleancode/scaga/Start.java
@@ -1,5 +1,7 @@
 package app.cleancode.scaga;
 
+import app.cleancode.scaga.engine.annotations.ImportGameObject;
+
 public class Start {
 public static void main(String[] args) {
 	GameStart.begin(args);
