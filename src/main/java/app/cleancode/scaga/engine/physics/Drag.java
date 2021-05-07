e63f2b243f9dbf8bb13e9bc03e41ce9998e18b94
Jett Thompson <jett.thompson.59@gmail.com>
2021-05-08 09:24:44 +1000

    made drag follow the same rules as movement and gravity (pixels per second)

--- src/main/java/app/cleancode/scaga/engine/physics/Drag.java
+++ src/main/java/app/cleancode/scaga/engine/physics/Drag.java
@@ -1,5 +1,8 @@
 package app.cleancode.scaga.engine.physics;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import app.cleancode.scaga.engine.GameObject;
 import app.cleancode.scaga.engine.PhysicalLaw;
 import app.cleancode.scaga.engine.events.StopEvent;
@@ -7,33 +10,46 @@ import javafx.scene.Node;
 
 public class Drag extends PhysicalLaw {
 
+	private final Map<GameObject<? extends Node>, Long> lastDragTimes;
+
+	public Drag() {
+		lastDragTimes = new HashMap<>();
+	}
+
 	@Override
 	public void handle(GameObject<Node> obj) {
+		if (lastDragTimes.containsKey(obj)) {
+			long lastDragTime = lastDragTimes.get(obj);
+			long currentTime = System.nanoTime();
+			long delta = currentTime - lastDragTime;
+			double dragAmount = delta / 1000000000d * obj.drag;
 		if (obj.isTouchingGround) {
 			if (obj.xVelocity < 0) {
-				obj.xVelocity = Math.min(0, obj.xVelocity + obj.drag);
+				obj.xVelocity = Math.min(0, obj.xVelocity + dragAmount);
 				if (obj.xVelocity == 0) {
 					obj.handleEvent(new StopEvent());
 				}
 			}else if (obj.xVelocity > 0) {
-				obj.xVelocity = Math.max(0, obj.xVelocity - obj.drag);
+				obj.xVelocity = Math.max(0, obj.xVelocity - dragAmount);
 				if (obj.xVelocity == 0) {
 					obj.handleEvent(new StopEvent());
 				}
 			}
 		}else {
 			if (obj.xVelocity < 0) {
-				obj.xVelocity = Math.min(0, obj.xVelocity + obj.drag / 2d);
+				obj.xVelocity = Math.min(0, obj.xVelocity + dragAmount / 2d);
 				if (obj.xVelocity == 0) {
 					obj.handleEvent(new StopEvent());
 				}
 			}else if (obj.xVelocity > 0) {
-				obj.xVelocity = Math.max(0, obj.xVelocity - obj.drag / 2d);
+				obj.xVelocity = Math.max(0, obj.xVelocity - dragAmount / 2d);
 				if (obj.xVelocity == 0) {
 					obj.handleEvent(new StopEvent());
 				}
 			}
 		}
+		}
+		lastDragTimes.put(obj, System.nanoTime());
 	}
 
 }
