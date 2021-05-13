296e03edd4104efe487178e63b86239b18d3cde8
Jett Thompson <jett.thompson.59@gmail.com>
2021-05-13 16:40:23 +1000

    made barel creation based on pressing and releasing the button rather than one per second

--- src/main/java/app/cleancode/scaga/listeners/barrel/BarrelCreationListener.java
+++ src/main/java/app/cleancode/scaga/listeners/barrel/BarrelCreationListener.java
@@ -7,20 +7,20 @@ import app.cleancode.scaga.engine.annotations.ImportGameObject;
 import app.cleancode.scaga.engine.keyboard.KeyBindings;
 
 public class BarrelCreationListener extends GameListener {
+private boolean keyState;
 
 	@ImportGameObject
 	public GameObject<?> barrel;
 
-	private long lastBarrel = -1;
-
 	@Override
 	public void update(State state) {
 		if (state.keyState.isKeyDown(KeyBindings.CREATE_BARREL)) {
-			long now = System.nanoTime();
-			if (now >= lastBarrel + 1000000000) {
-			state.createGameObject(barrel, Math.random(), 0);
-			lastBarrel = now;
+			if (!keyState) {
+			state.createGameObject(barrel, Math.random(), -5);
+			keyState = true;
 			}
+		}else {
+			keyState = false;
 		}
 	}
 
