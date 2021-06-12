03d8546dca9d4becf31fa0f8f0f0adc1a55dd28d
Jett Thompson <jett.thompson.59@gmail.com>
2021-06-12 18:00:29 +1000

    added the intersection shape (as a Bound) to the collision event

--- engine/src/main/java/app/cleancode/scaga/engine/events/CollisionEvent.java
+++ engine/src/main/java/app/cleancode/scaga/engine/events/CollisionEvent.java
@@ -1,12 +1,15 @@
 package app.cleancode.scaga.engine.events;
 
+import app.cleancode.scaga.bounds.Bound;
 import app.cleancode.scaga.collisions.Collidable;
 
 public class CollisionEvent implements Event {
     public final Collidable other;
+    public final Bound collisionBound;
 
-    public CollisionEvent(Collidable other) {
+    public CollisionEvent(Collidable other, Bound collisionBound) {
         this.other = other;
+        this.collisionBound = collisionBound;
     }
 
     @Override
--- engine/src/main/java/app/cleancode/scaga/engine/physics/Collisions.java
+++ engine/src/main/java/app/cleancode/scaga/engine/physics/Collisions.java
@@ -3,11 +3,13 @@ package app.cleancode.scaga.engine.physics;
 import java.util.HashSet;
 import java.util.Set;
 
+import app.cleancode.scaga.bounds.Bound;
 import app.cleancode.scaga.colliders.PolygonCollider;
 import app.cleancode.scaga.collisions.Collidable;
 import app.cleancode.scaga.engine.GameObject;
 import app.cleancode.scaga.engine.events.CollisionEvent;
 import app.cleancode.scaga.shape.polygon.Polygon2D;
+import javafx.geometry.Bounds;
 import javafx.scene.shape.Shape;
 
 public class Collisions {
@@ -24,9 +26,12 @@ public class Collisions {
                 Polygon2D objectRegion = object.getRegion();
                 Shape intersection = PolygonCollider.intersect(objRegion, objectRegion);
                 if (!intersection.getBoundsInLocal().isEmpty()) {
-                    obj.handleEvent(new CollisionEvent(object));
+                    Bounds intersectionBounds = intersection.getBoundsInParent();
+                    Bound collisionBound = new Bound(intersectionBounds.getMinX(), intersectionBounds.getMinY(),
+                            intersectionBounds.getWidth(), intersectionBounds.getHeight());
+                    obj.handleEvent(new CollisionEvent(object, collisionBound));
                     if (object instanceof GameObject<?>) {
-                        ((GameObject<?>) object).handleEvent(new CollisionEvent(obj));
+                        ((GameObject<?>) object).handleEvent(new CollisionEvent(obj, collisionBound));
                     }
                     return intersection;
                 }
