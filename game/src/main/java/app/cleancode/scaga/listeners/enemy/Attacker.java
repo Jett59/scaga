eb83612fbb642cdb5e5417beb8a37d9424cc8c0e
Jett Thompson <jett.thompson.59@gmail.com>
2021-07-22 14:32:13 +1000

    added the ability for enemies to attack the player

---
+++ game/src/main/java/app/cleancode/scaga/listeners/enemy/Attacker.java
@@ -0,0 +1,34 @@
+package app.cleancode.scaga.listeners.enemy;
+
+import app.cleancode.scaga.engine.GameListener;
+import app.cleancode.scaga.engine.GameObject;
+import app.cleancode.scaga.engine.GameProperty;
+import app.cleancode.scaga.engine.State;
+import app.cleancode.scaga.engine.annotations.AttachedTo;
+import app.cleancode.scaga.engine.annotations.ImportGameObject;
+import app.cleancode.scaga.engine.annotations.ImportGameProperty;
+
+@AttachedTo("enemy")
+public class Attacker extends GameListener {
+
+    @ImportGameObject
+    public GameObject<?> enemy;
+
+    @ImportGameProperty(owner = "enemy")
+    public GameProperty attacking;
+
+    @Override
+    public void update(State state) {
+        if (enemy.xVelocity == 0) {
+            attacking.set(true);
+        } else {
+            attacking.set(false);
+        }
+    }
+
+    @Override
+    public void startup(State state) {
+        attacking.set(false);
+    }
+
+}
---
+++ game/src/main/java/app/cleancode/scaga/listeners/player/DamageDealer.java
@@ -0,0 +1,39 @@
+package app.cleancode.scaga.listeners.player;
+
+import app.cleancode.scaga.bounds.Bound;
+import app.cleancode.scaga.collisions.Collidable;
+import app.cleancode.scaga.engine.GameListener;
+import app.cleancode.scaga.engine.GameProperty;
+import app.cleancode.scaga.engine.State;
+import app.cleancode.scaga.engine.annotations.AttachedTo;
+import app.cleancode.scaga.engine.annotations.ImportGameProperty;
+
+@AttachedTo("player")
+public class DamageDealer extends GameListener {
+    public static double ATTACK_AMOUNT = 0.002;
+
+    @ImportGameProperty(owner = "enemy")
+    public GameProperty attacking;
+    @ImportGameProperty(owner = "player")
+    public GameProperty health;
+
+    @Override
+    public void update(State state) {
+
+    }
+
+    @Override
+    public void startup(State state) {
+
+    }
+
+    @Override
+    public void onCollision(Collidable other, Bound collisionBound) {
+        if (other.toString().equals("enemy")) {
+            if (attacking.getBoolean()) {
+                health.set(health.getDouble() - ATTACK_AMOUNT);
+            }
+        }
+    }
+
+}
